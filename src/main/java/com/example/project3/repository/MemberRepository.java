2d7c5524e1de8a9fd8e30b664cc75742751dbdde
PQDuy <141241165+PQDuy@users.noreply.github.com>
2024-05-16 23:42:39 +0700

    Add files via upload

--- src/main/java/com/example/project3/repository/MemberRepository.java
+++ src/main/java/com/example/project3/repository/MemberRepository.java
@@ -1,12 +1,31 @@
 package com.example.project3.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.example.project3.entity.Member;
 
+@EnableJpaRepositories
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Integer>{
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+  @Query("SELECT m FROM Member m WHERE m.MaTV = :MaTV AND m.Password = :Password")
+  Optional<Member> findOneByIdAndPassword(int MaTV, String Password);
 
-    
+  @Query("SELECT m FROM Member m WHERE m.Email = :Email")
+  Member findByEmail(String Email);
+
+  @Query("SELECT m FROM Member m WHERE m.MaTV = :MaTV")
+  boolean existsByMaTV(int MaTV);
+
+  @Query("SELECT m FROM Member m WHERE m.Email = :Email")
+  boolean existsByEmail(String Email);
+
+  @Query("SELECT m FROM Member m WHERE m.SDT = :SDT")
+  boolean existsBySDT(String SDT);
+
+  public Member findByResetPasswordToken(String token);
 }
