a30ff612b425d0ee089f1ed99b7be9b2776e3925
PQDuy <141241165+PQDuy@users.noreply.github.com>
2024-05-16 23:42:17 +0700

    Add files via upload

--- src/main/java/com/example/project3/entity/Member.java
+++ src/main/java/com/example/project3/entity/Member.java
@@ -1,43 +1,112 @@
 package com.example.project3.entity;
-import jakarta.persistence.*;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "thanhvien")
 public class Member {
-     @Id
-    @Column(name = "MaTV")
-    private int MaTV;
+  @Id
+  @Column(name = "MaTV")
+  private int MaTV;
 
-    @Column(name = "Hoten")
-    private String HoTen;
+  @Column(name = "Hoten")
+  private String HoTen;
 
-    @Column(name = "Khoa")
-    private String Khoa;
+  @Column(name = "Khoa")
+  private String Khoa;
 
-    @Column(name = "Nganh")
-    private String Nganh;
+  @Column(name = "Nganh")
+  private String Nganh;
 
-    @Column(name = "SDT")
-    private String SDT;
+  @Column(name = "SDT")
+  private String SDT;
 
-    @Column(name = "Email")
-    private String Email;
+  @Column(name = "Email")
+  private String Email;
 
-    @Column(name = "Password")
-    private String Password;
+  @Column(name = "Password")
+  private String Password;
 
-    @Column(name = "reset_password_token")
-    private String resetPasswordToken;
+  @Column(name = "reset_password_token")
+  private String resetPasswordToken;
 
-    // @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, mappedBy = "thanhVien",fetch = FetchType.LAZY)
-    // private List<Thongtinsd> usages;
-    
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "thanhVien")
-    private List<Thongtinsd> usages;
+  public Member(int maTV, String hoTen, String khoa, String nganh, String sDT, String Email, String password) {
+    this.MaTV = maTV;
+    this.HoTen = hoTen;
+    this.Khoa = khoa;
+    this.Nganh = nganh;
+    this.SDT = sDT;
+    this.Email = Email;
+    this.Password = password;
+  }
+
+  public Member() {
+  }
+
+  public int getMaTV() {
+    return this.MaTV;
+  }
+
+  public void setMaTV(int maTV) {
+    this.MaTV = maTV;
+  }
+
+  public String getHoTen() {
+    return this.HoTen;
+  }
+
+  public void setHoTen(String hoTen) {
+    this.HoTen = hoTen;
+  }
+
+  public String getKhoa() {
+    return this.Khoa;
+  }
+
+  public void setKhoa(String khoa) {
+    this.Khoa = khoa;
+  }
+
+  public String getNganh() {
+    return this.Nganh;
+  }
+
+  public void setNganh(String nganh) {
+    this.Nganh = nganh;
+  }
+
+  public String getSDT() {
+    return this.SDT;
+  }
+
+  public void setSDT(String sDT) {
+    this.SDT = sDT;
+  }
+
+  public String getEmail() {
+    return this.Email;
+  }
+
+  public void setEmail(String Email) {
+    this.Email = Email;
+  }
+
+  public String getPassword() {
+    return this.Password;
+  }
+
+  public void setPassword(String password) {
+    this.Password = password;
+  }
+
+  public String getResetPasswordToken() {
+    return resetPasswordToken;
+  }
+
+  public void setResetPasswordToken(String resetPasswordToken) {
+    this.resetPasswordToken = resetPasswordToken;
+  }
 }
